32282ee33ddc008d1e60f68cb264fbee52b2c3fc
mohaa <mohaa.r.6789@gmail.com>
2022-06-17 21:44:44 +0530

    removing file as its not required

--- pages/api/vegetarian/[detail].ts
+++
@@ -1,8 +0,0 @@
-const details = {}
-
-export default function handler(req, res) {
-  if (req.method === 'GET') {
-    console.log(req.url)
-    res.status(200).json(details)
-  }
-}
--- pages/api/vegetarian/index.ts
+++
@@ -1,8 +0,0 @@
-const vegetables = []
-
-export default function handler(req, res) {
-  if (req.method === 'GET') {
-    console.log(req.url)
-    res.status(200).json(vegetables)
-  }
-}
