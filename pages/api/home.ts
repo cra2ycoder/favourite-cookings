93974a629d4f2c2f4209ae4f7eb0d57d4bd8322b
mohaa <mohaa.r.6789@gmail.com>
2022-06-17 22:07:48 +0530

    url updated with .env values

--- pages/api/home.ts
+++ pages/api/home.ts
@@ -1,10 +1,11 @@
 export default async function handler(req, res) {
   if (req.method === 'GET' && !req.url.includes('favicon.ico')) {
-    console.log(req.url)
+    // console.log(req.url)
+    const playlistId = process.env.YOUTUBE_PLAYLIST_ID
+    const apiKey = process.env.YOUTUBE_API_KEY
 
     const results = await fetch(
-      // 'https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet%2CcontentDetails&playlistId=PLTePkUcpu5A4PCt_O9V9X1B_XCviJCCFy&key=AIzaSyCaw1C_dhNQ18bz_rBB-3aQhLzkRjB2VZ8',
-      'https://www.googleapis.com/youtube/v3/playlistItems?part=snippet,contentDetails&maxResults=25&playlistId=PLTePkUcpu5A4PCt_O9V9X1B_XCviJCCFy&key=AIzaSyCaw1C_dhNQ18bz_rBB-3aQhLzkRjB2VZ8',
+      `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet,contentDetails&maxResults=25&playlistId=${playlistId}&key=${apiKey}`,
       { method: 'GET' }
     )
       .then(x => x.json())
