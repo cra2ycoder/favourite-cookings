f46f47f399d10813463db2333375a0d70b9ca759
mohaa <mohaa.r.6789@gmail.com>
2022-06-24 00:25:30 +0530

    filter enhancements
    - all support added
    - chef filtering added

--- utils/index.ts
+++ utils/index.ts
@@ -1,6 +1,7 @@
 export const fetcher = url => fetch(url).then(res => res.json())
 
 export const recipeMap = new Map()
+recipeMap.set('All', [])
 recipeMap.set('Veg Biryani', ['vegetable', 'biryani'])
 recipeMap.set('Non-Veg Biryani', ['mutton', 'chicken', 'egg', 'biryani'])
 recipeMap.set('Veg Gravy', ['mushroom', 'gravy'])
@@ -10,6 +11,10 @@ recipeMap.set('Poriyal', ['kootu', 'poriyal'])
 recipeMap.set('Kuzhambu', ['kulambu', 'curry', 'sambar', 'kuzhambu'])
 recipeMap.set('Rasam', ['rasam'])
 recipeMap.set('Dry', ['dry'])
+recipeMap.set('Fry', ['fry'])
+
+export const chefMap = new Map()
+chefMap.set('All', [])
 
 export const getRecipeFilters = () => {
   let arr = []
@@ -19,21 +24,47 @@ export const getRecipeFilters = () => {
   return arr
 }
 
-export const filterDataByMap = (key, items = []) => {
-  return items.filter(curItem => {
-    const {
-      snippet: { title = '' },
-    } = curItem
+export const setChefMapByData = (list = []) => {
+  list.forEach(x => {
+    let title = x?.snippet?.videoOwnerChannelTitle
+    if (title) {
+      chefMap.set(title, [title])
+    }
+  })
+}
 
-    const words = recipeMap.get(key)
+export const getChefFilters = () => {
+  let arr = []
+  chefMap.forEach((value, key) => {
+    arr.push(key)
+  })
+  return arr
+}
+
+export const filterDataByMap = (key, filterMap, items = [], property = '') => {
+  return items.filter(curItem => {
+    const { snippet = {} } = curItem
+    const compareProp = snippet?.[property || 'title']
+
+    const words = filterMap.get(key)
+
+    if (key === 'All' && words.length === 0) {
+      return curItem
+    }
+
     let regex = new RegExp(
-      '\\b(' + words.slice(0, words.length - 1).join('|') + ')\\b',
+      '\\b(' +
+        words
+          .slice(0, words.length > 1 ? words.length - 1 : words.length)
+          .join('|')
+          .toLowerCase() +
+        ')\\b',
       'g'
     )
 
     return (
-      regex.test(title.toLowerCase()) &&
-      title.toLowerCase().includes(words[words.length - 1])
+      regex.test(compareProp.toLowerCase()) &&
+      compareProp.toLowerCase().includes(words[words.length - 1].toLowerCase())
     )
   })
 }
